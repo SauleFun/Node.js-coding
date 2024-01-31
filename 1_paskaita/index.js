048b33737563eaf00314d25b027ef5a3bcb502f4
SauleleFun <jurgita.dulkyte@gmail.com>
2024-01-31 15:27:32 +0200

    came back to original 2nd lesson requests

--- 1_paskaita/index.js
+++ 1_paskaita/index.js
@@ -1,9 +1,7 @@
 const express = require("express"); // importuojam express
-const cors = require("cors"); // importuojam cors moduli
+
 const app = express(); // sukuriam express aplikaciją
 const port = 3000; // uostas
-
-app.use(cors()); // pritaikom CORS taisykles
 
 // sukuriam route "/" (kelią) kuriuo užėjus grąžinsim (GET metodas) tekstą "Hello world"
 app.get("/", (req, res) => {
@@ -24,14 +22,19 @@ app.get("/students", (req, res) => {
   res.send(students);
 });
 
-// // 1 užduotis
-const users = ["Alex", "Rose", "Megan"];
-
-app.get("/users", (req, res) => {
-  res.send(users);
-});
-
 // paleidžiama (klausosi) aplikaciją ant porto(3000)
 app.listen(port, () => {
   console.log(`App is listening on the port ${port}`);
 });
+
+// // // 1 užduotis
+// const users = ["Alex", "Rose", "Megan"];
+
+// app.get("/users", (req, res) => {
+//   res.send(users);
+// });
+
+// // paleidžiama (klausosi) aplikaciją ant porto(3000)
+// app.listen(port, () => {
+//   console.log(`App is listening on the port ${port}`);
+// });
