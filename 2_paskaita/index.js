df187f8093aad725c4533f2c9a806a4753b706f4
SauleleFun <jurgita.dulkyte@gmail.com>
2024-02-01 21:35:21 +0200

    panaudojome dinamini route

--- 2_paskaita/index.js
+++ 2_paskaita/index.js
@@ -16,6 +16,23 @@ app.get("/brands", (req, res) => {
   res.send(brands);
 });
 
+// /brands/B => ["BMW"];
+// /brands/g => [];
+// /brands/b =>
+// dinaminis route
+app.get("/brands/:firstLetter", (req, res) => {
+  // grazinti visus brandus kurie prasideda duota raide
+  console.log(req.params);
+  // const firstLetter = req.params.firstLetter; old way/another way
+  const { firstLetter } = req.params; // su dvitaskiu einantys parameterai (dinaminiai laukai);
+  console.log(firstLetter);
+  const filteredBrands = brands.filter(
+    (brand) => brand[0].toLowerCase() === firstLetter.toLowerCase()
+  );
+
+  res.send(brands);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
